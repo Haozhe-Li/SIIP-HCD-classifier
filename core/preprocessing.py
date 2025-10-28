52d6ed2dd0c75b4f5a3ef59564f5355c065c1f75
Haozhe-Li <hao-zhe.li@outlook.com>
2025-10-27 20:00:07 -0500

    refactor: remove hardcoded OCR language option from pipeline settings

--- core/preprocessing.py
+++ core/preprocessing.py
@@ -28,7 +28,6 @@ class PreProcessor:
         pipeline_options.do_ocr = True
         pipeline_options.do_table_structure = True
         pipeline_options.table_structure_options.do_cell_matching = True
-        pipeline_options.ocr_options.lang = ["es"]
         pipeline_options.accelerator_options = AcceleratorOptions(
             num_threads=4, device=AcceleratorDevice.AUTO
         )
